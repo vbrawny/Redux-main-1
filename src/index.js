d3b55cebcb14b474bcb0cbb74a43206193511980
venky <vbrawny@users.noreply.github.com>
2021-08-01 22:01:45 +0100

    action creators

--- src/index.js
+++ src/index.js
@@ -11,17 +11,24 @@ const initialState = { value: 0 };
 
 // const INCREMENT = "counter/increment";
 const INCREMENT = "increment";
+const ADDAMOUNT = "addAmount";
 
 const incrementAction = { type: INCREMENT };
+
+const addAmountAction = (amount) => ({ type: ADDAMOUNT, payload: amount });
 
 const reducer = (state = initialState, action) => {
   if (action.type === INCREMENT) {
     return { ...state, value: state.value + 1 };
+  }
+  if (action.type === ADDAMOUNT) {
+    return { ...state, value: action.payload };
   }
   return state;
 };
 
 const store = createStore(reducer);
 store.dispatch(incrementAction);
+store.dispatch(addAmountAction(7));
 
 console.log(store.getState());
