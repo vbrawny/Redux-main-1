bf8e0d74de636388599aac0f1f44fdcaca48b61b
venky <vbrawny@users.noreply.github.com>
2021-08-01 18:15:21 +0100

    added changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+// The below are the main APIs of Redux, but on a total we have 9 of them.
 import {
   createStore,
   compose,
+  combineReducers,
   applyMiddleware,
   bindActionCreators
 } from "redux";
